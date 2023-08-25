abb1311d6ecb8d9b56053115fc482631ae5077ba
Utkarsh Jaiswal <91864472+utkarshml@users.noreply.github.com>
2023-08-25 12:15:06 +0530

    Update database.js

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
  const mongodb =(next) =>{ 
-     mongoose.connect("mongodb://127.0.0.1:27017" , {
+     mongoose.connect(process.env.MONGO, {
     dbName:"PortfolioUser"
 }).then((data)=>{
     console.log("Database is connected")
